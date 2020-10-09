9d4589191f3d1e0571e56a2c1a54c35a93b7c281
okhater <71776758+okhater@users.noreply.github.com>
2020-10-09 16:51:20 +0300

    Update router.php

--- router.php
+++ router.php
@@ -6,6 +6,8 @@
     require_once("reg.php");
   }else if($uri[1]=="auth"){
     require_once("auth.php");
+  }else if($uri[1]=="lk"){
+    require_once("lk.php");    
   }else if($uri[1] == "addUser"){
     User::addUser();
   }else if($uri[1] == "authUser"){
